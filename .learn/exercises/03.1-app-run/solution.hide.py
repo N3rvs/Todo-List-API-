71394a4bdeec3d3809007718627f4327e7c7ff97
Guillermo Longueira <nervelessdev@gmail.com>
2023-06-26 09:01:57 +0000

    vamo

--- .learn/exercises/03.1-app-run/solution.hide.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-# These two lines should always be at the end of your app.py file.
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True)
